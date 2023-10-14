8bd3c196c36f1b818549c734456e7f1ddb34e930
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-14 22:31:28 +0800

    added new views and urls

--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World')
+
+def brian(request):
+    return HttpResponse('Hello, Brian')
+
+def david(request):
+    return HttpResponse('Hello, David')
