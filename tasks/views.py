32e8742a827df6ac2fccdc02471e670aba0a4c88
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-18 15:18:00 +0800

    dynamic task add

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,13 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+tasks = [] 
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -18,7 +20,8 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            tasks.append(task)        
+            return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
